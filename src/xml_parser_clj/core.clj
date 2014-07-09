dba502ef2444a7ecda2874144397d2900b0e4c5a
kaz-yos <kazky@mac.com>
2014-07-09 15:45:08 +0900

    Remove test code

--- src/xml_parser_clj/core.clj
+++ src/xml_parser_clj/core.clj
@@ -1,4 +1,7 @@
 ;;; Main file to parse XML
+
+;;;
+;;; Namespace and dependencies
 (ns xml-parser-clj.core
   (:require [clojure.xml :as xml]
             [clojure.zip :as zip]
@@ -14,23 +17,17 @@
 ;;;
 ;;; Code
 
+
+;;; Define a function to convert an XML string to clojure internal representation
 ;; convenience function, first seen at nakkaya.com later in clj.zip src
 (defn zip-str
-  "convenience function, first seen at nakkaya.com later in clj.zip src"
+  "convert an XML string to clojure internal representation.
+
+convenience function, first seen at nakkaya.com later in clj.zip src"
   [s]
   (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
 
-;;;
-;;; Load data
-;; Load PubMed XML
-(def test-xml (slurp "/Users/kazuki/Documents/BWH/_Project_RheumPharm/_tools/pubmed.test.xml"))
-;; Load as a map
-(def test-formatted (first (zip-str test-xml)))
-
-
-;;;
-;;; Using :tag based search
 
 ;;; Define a recursive function to look for target :tag
 (defn recur-search
@@ -52,15 +49,6 @@
    :else []))
 
 
-;;; Simple elements
-(flatten (recur-search :PMID test-formatted))
-(flatten (recur-search :ArticleTitle test-formatted))
-
-
-;;; Abstract (multiple subelements)
-(map flatten (recur-search :Abstract test-formatted))
-
-
 ;;; Define a function to concatenate abstract
 (defn concat-abstract
   "Concatenate abstract elements from an abstract."
@@ -72,16 +60,6 @@
        ;; Make it a single string
        (apply str,  )))
 
-
-(->> (recur-search :Abstract test-formatted)
-     ;; Flatten within each article
-     (map flatten,  )
-     ;; Concatenate elements within each article
-     (map concat-abstract,  ))
-
-
-;;; Date (require conversion)
-(first (map flatten (recur-search :PubDate test-formatted)))
 
 ;;; Define a function to get :content by :tag
 (defn content-by-tag
@@ -107,9 +85,6 @@
        (map str,  )
        (apply str,  )))
 
-(padding "0" 2 "12")
-(padding "0" 2  "1")
-
 
 ;;; Define a function to create R style date string
 (defn date-string
@@ -130,12 +105,8 @@
          (if (= "00" day-pad)   "00"   day-pad))))
 
 
-(map date-string (map flatten (recur-search :PubDate test-formatted)))
-
-
-
 ;;;
-;;; Combine output
+;;; Define a function to combine output
 (defn parse-pubmed
   "Parse PubMed XML map (PubmedArticle) to format appropriate for CSV"
   [PubmedArticle]
@@ -152,64 +123,18 @@
          PubDate)))
 
 
-(parse-pubmed test-formatted)
-(count (parse-pubmed test-formatted))
-(first (parse-pubmed test-formatted))
-
-
-
-
-;;;
-;;; Test on actual data
-;; Load PubMed XML
-(def ra-bio-inf-xml (slurp "/Users/kazuki/Documents/BWH/_Project_RheumPharm/20140709-RA-bDMARDs-Infection.xml"))
-;;
-;; Delete these lines
-;; <?xml version="1.0"?>
-;; <!DOCTYPE PubmedArticleSet PUBLIC "-//NLM//DTD PubMedArticle, 1st January 2014//EN" "http://www-ncbi-nlm-nih-gov.ezp-pqrod1.hul.harvard.edu/corehtml/query/DTD/pubmed_140101.dtd">
-;;
-;; Load as a map
-(def ra-bio-inf-map (first (zip-str ra-bio-inf-xml)))
-
-
-;; Work at the article level
-(def parsed-pubmed (map parse-pubmed (:content ra-bio-inf-map)))
-
-(class parsed-pubmed)
-(count parsed-pubmed)
-(map count parsed-pubmed)
-(filter #(zero? (count %)) parsed-pubmed)
-
-(nth parsed-pubmed 37)
-(nth parsed-pubmed 38) ; This one lacks abstract
-(nth parsed-pubmed 39)
-(nth parsed-pubmed 40)
-
-(first (map first parsed-pubmed))
-
-
-
-;;;
-;;; Sepcify a directory for conversion
-;; http://clojuredocs.org/clojure_core/clojure.core/file-seq
-;; http://stackoverflow.com/questions/8566531/listing-files-in-a-directory-in-clojure
-(def directory (io/file "/Users/kazuki/Documents/BWH/_Project_RheumPharm/_tools/xml-to-csv-dir"))
-(def files (file-seq directory))
-(take 10 files)
-
-
-
 ;;;
 ;;; -main function
 (defn -main
   "-main function to be called when the program is run"
-  [pm-xml-file-path]
+  []
   ;;
-  (let [parsed-pm (->> (slurp pm-xml-file-path)
-                       (zip-str,  )
-                       (first,  )
-                       (:conent,  )
-                       (map parse-pubmed,  ))]
+  (let [pm-xml-file-path (first *command-line-args*)
+        parsed-pm        (->> (slurp pm-xml-file-path)
+                              (zip-str,  )
+                              (first,  )
+                              (:conent,  )
+                              (map parse-pubmed,  ))]
     ;;
     ;; write to a csv file
     (with-open [out-file (io/writer (str pm-xml-file-path ".csv"))]
