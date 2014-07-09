49ca7bd0f9497b08a9c4f63c6d548dc9a83070db
kaz-yos <kazky@mac.com>
2014-07-09 09:09:53 +0900

    Add clojure.xml/zip to ns :require

--- src/xml_parser_clj/core.clj
+++ src/xml_parser_clj/core.clj
@@ -1,5 +1,16 @@
-(ns xml-parser-clj.core)
+;;; Main file to parse XML
+(ns xml-parser-clj.core
+  (:require [clojure.xml :as xml]
+            [clojure.zip :as zip]))
 
+
+;;;
+;;; References
+;; ClojureDocs parse: http://clojuredocs.org/clojure_core/clojure.xml/parse
+
+
+;;;
+;;; Code
 (defn foo
   "I don't do a whole lot."
   [x]
