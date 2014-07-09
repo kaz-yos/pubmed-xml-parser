8c3eeee98b78045ace8b14294e825656055ff719
kaz-yos <kazky@mac.com>
2014-07-09 12:01:48 +0900

    Change recursive function not to return nil

--- src/xml_parser_clj/core.clj
+++ src/xml_parser_clj/core.clj
@@ -19,100 +19,16 @@
   (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
 
-;;parse from xml-strings to internal xml representation
-(zip-str "<a href='nakkaya.com'/>")
-
-;;root can be rendered with xml/emit-element
-(xml/emit-element (zip/root [{:tag :a, :attrs {:href "nakkaya.com"}, :content nil} nil]))
-
-(xml/emit-element (zip/root (zip-str "<a href='nakkaya.com'/>")))
-;;printed (to assure it's not lazy and for performance), can be caught to string variable with with-out-str
-
-
 ;;;
 ;;; Load data
+;; Load PubMed XML
 (def test-xml (slurp "/Users/kazuki/Documents/BWH/_Project_RheumPharm/_tools/pubmed.test.xml"))
-(class test-xml)
-(println test-xml)
-
+;; Load 
 (def test-formatted (zip-str test-xml))
-
-(class test-formatted)
-
-(count test-formatted)
-(first test-formatted)
-(last test-formatted)
-
-;; :PubmedArticleSet is a vector
-(:tag (first test-formatted))
-(class (:content (first test-formatted)))
-
-
-;; :PubmedArticle
-(:tag (first (:content (first test-formatted))))
-(class (:content (first (:content (first test-formatted)))))
-
-
-;; :MedlineCitation
-(:tag (first (:content (first (:content (first test-formatted))))))
-(class (:content (first (:content (first (:content (first test-formatted)))))))
-;; Elements are maps
-(map class (:content (first (:content (first (:content (first test-formatted)))))))
-
-(map count (:content (first (:content (first (:content (first test-formatted)))))))
-
-
-;; Check :tag on all vector elements
-(map :tag (:content (first (:content (first (:content (first test-formatted)))))))
-
-;; :PMID
-(:tag (get (:content (first (:content (first (:content (first test-formatted)))))) 0))
-(:content (get (:content (first (:content (first (:content (first test-formatted)))))) 0))
-
-(:content (get (:content (first (:content (first (:content (first test-formatted)))))) 0))
-
-;; Define a helper function
-(defn fst-cont
-  "Get the :content element of the first vector element"
-  [vctr]
-  (:content (first vctr)))
-
-(first (:content (get (fst-cont (fst-cont (fst-cont test-formatted))) 0)))
-
-
-;; :DateCreated
-(:tag (get (:content (first (:content (first (:content (first test-formatted)))))) 1))
-(map :tag (:content (get (:content (first (:content (first (:content (first test-formatted)))))) 1)))
-
-;; :Article
-(:tag (get (:content (first (:content (first (:content (first test-formatted)))))) 2))
-(map :tag (:content (get (:content (first (:content (first (:content (first test-formatted)))))) 2)))
-
-(get (:content (get (:content (first (:content (first (:content (first test-formatted)))))) 2)) 1)
-
-
-;; Filter based on :tag
-(filter #(= (:tag %) :ArticleTitle) (:content (get (:content (first (:content (first (:content (first test-formatted)))))) 2)))
-
 
 
 ;;;
 ;;; Using :tag based search
-;;; Define a function to filter a vector based on :tag
-(defn filter-tag
-  "Filter vector elements based on :tag values, and return :content"
-  [tag a-seq]
-  (map :content (filter #(= (:tag %) tag) a-seq)))
-
-;; Get a vector element with :tag :PubmedArticleSet
-(filter-tag :PubmedArticleSet test-formatted)
-(count (filter-tag :PubmedArticleSet test-formatted))
-(first (filter-tag :PubmedArticleSet test-formatted))
-;; Two :PubmedArticle's are contained
-;; The recursive search should be mapped to these
-(count (first (filter-tag :PubmedArticleSet test-formatted)))
-
-
 
 ;;; Define a recursive function to look for target :tag
 (defn recur-search
@@ -123,43 +39,22 @@
    ;; if tag matches :ta-map
    (= tag (:tag a-map)) (:content a-map)
    ;; if it does not match, and all vector elements are maps
-   (every? map? (:content a-map)) (for [elt (:content a-map)]
-                                    (recur-search tag elt))
-   ;; otherwise return nil
-   :else nil))
+   (every? map? (:content a-map)) (->> (for [elt (:content a-map)]
+                                         ;; Recurse on each element
+                                         (recur-search tag elt))
+                                       ;; Drop empty elements
+                                       (filter (complement empty?),  ))
+   ;; otherwise return an empty vector
+   :else []))
 
 ;; works at top level
-(recur-search :PubmedArticleSet (first test-formatted))
+;; (recur-search :PubmedArticleSet (first test-formatted))
 ;; 
 ;; works at deeper levels, but produces garbage
 (recur-search :PMID (first test-formatted))
+(filter (complement nil?) (flatten (recur-search :PMID (first test-formatted))))
+
 (recur-search :ArticleTitle (first test-formatted))
+(recur-search :Abstract (first test-formatted))
 
-
-;; Get a vector using :content
-;; and filter elements with :tag :PubmedArticle
-(filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted)))
-(count (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted))))
-
-
-
-
-
-;; map to seq of seq
-(map #(filter-tag :MedlineCitation %) (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted))))
-
-(first (first (map #(filter-tag :MedlineCitation %) (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted))))))
-
-(count (first (first (map #(filter-tag :MedlineCitation %) (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted)))))))
-
-(map #(filter-tag :PMID %) (first (first (map #(filter-tag :MedlineCitation %) (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted)))))))
-
-
-;;
-(map #(filter-tag :ArticleTitle %) (map #(filter-tag :MedlineCitation %) (filter-tag :PubmedArticle (first (filter-tag :PubmedArticleSet test-formatted)))))
-
-
-
-
-
-
+(flatten (recur-search :Abstract (first test-formatted)))
