745259cf1e204ccd7b929a3e917fe03f43837002
kaz-yos <kazky@mac.com>
2014-07-09 16:06:38 +0900

    Add entry point to project.clj file

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject xml-parser-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "PubMed XML to CSV converter"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main xml-parser-clj.core
+  :aot [xml-parser-clj.core]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
                  ;; https://github.com/clojure/data.csv
