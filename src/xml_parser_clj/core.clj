2e40fc3c2330ab19802bda3c9d32ac20be228acd
kaz-yos <kazky@mac.com>
2014-07-09 09:17:34 +0900

    Add XML string to internal representation converter

--- src/xml_parser_clj/core.clj
+++ src/xml_parser_clj/core.clj
@@ -11,7 +11,20 @@
 
 ;;;
 ;;; Code
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;; convenience function, first seen at nakkaya.com later in clj.zip src
+(defn zip-str
+  "convenience function, first seen at nakkaya.com later in clj.zip src"
+  [s]
+  (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+
+
+;;parse from xml-strings to internal xml representation
+(zip-str "<a href='nakkaya.com'/>")
+
+;;root can be rendered with xml/emit-element
+(xml/emit-element (zip/root [{:tag :a, :attrs {:href "nakkaya.com"}, :content nil} nil]))
+
+(xml/emit-element (zip/root (zip-str "<a href='nakkaya.com'/>")))
+;;printed (to assure it's not lazy and for performance), can be caught to string variable with with-out-str
+
